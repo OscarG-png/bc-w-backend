a826a21427e9cd51866d7652bbccf0964418d60f
OscarG <oscar.gamez88@gmail.com>
2024-10-10 14:28:41 -0700

    backend works

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
 import fastify from "fastify";
+require("dotenv").config();
 
 const server = fastify({ logger: true });
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
+// Define a simple route
 server.get("/", async (request, reply) => {
     return { message: "Hello, Fastify with TypeScript!" };
 });
 
 const start = async () => {
     try {
-        await server.listen({ port: 3000 });
-        console.log("Server listening on http://localhost:3000");
+        await server.listen({ port, host: "0.0.0.0" }); // Listen on all interfaces
+        console.log(`Server listening on http://localhost:${port}`); // Use the dynamic port
     } catch (err) {
         server.log.error(err);
         process.exit(1);
